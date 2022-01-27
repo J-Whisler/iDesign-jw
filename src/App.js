f878aaf39e65eaa6fdc4fa107eab5047bf0eb964
J-Whisler <jwhisler521@gmail.com>
2022-01-26 22:23:15 -0500

    initial file clean up, package installs, react-router set up, initial folder structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Main from './pages/Main/Main';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" exact element={<Main />} />
+        </Routes>
+     
+      </div>
+    </Router>
+  );
+}
+
+export default App;
