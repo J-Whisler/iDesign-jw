6ba766c01f0c53d266edbfbba9307176bc1aafbb
J-Whisler <jwhisler521@gmail.com>
2022-01-30 00:32:18 -0500

    page title component, about me title rendered

---
+++ src/components/PageTitle/PageTitle.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./PageTitle.scss";
+
+const PageTitle = ({ children }) => {
+  return <div className="pageTitle__container">{children}</div>;
+};
+
+export default PageTitle;
--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./AboutMe.scss";
 
+import TopNav from "../../components/TopNav/TopNav";
+import ParticleComponent from "../../components/Particles/ParticleComponent";
+import PageTitle from "../../components/PageTitle/PageTitle";
+
 const AboutMe = () => {
-  return <div className="AboutMe__container container">About Me</div>;
+  return (
+    <div className="aboutMe__container container">
+      <TopNav />
+      <ParticleComponent theme="theme5" />
+      <PageTitle>
+        <h2 className="aboutMe__Title">About Me</h2>
+      </PageTitle>
+    </div>
+  );
 };
 
 export default AboutMe;
